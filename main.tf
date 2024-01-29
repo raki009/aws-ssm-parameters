0a87564a48ed7d6dc4a215ce45ae87fc7db730ca
rakesh Guddeti <raki64846@gmail.com>
2024-01-29 21:51:21 +0530

    .

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_ssm_parameter" "parameters" {
+  for_each = var.parameters
+  name  = each.key
+  type  = "string"
+  value = each.value
+  key_id = var.key_id
+}
+
+resource "aws_ssm_parameter" "secrets" {
+  for_each = var.secrets
+  name  = each.key
+  type  = "SecureString"
+  value = each.value
+  key_id = var.key_id
+}
+
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "key_id" {
+  default = "arn:aws:kms:us-east-1:194840184236:key/071f905f-600f-4635-a78a-4052fef73d3a"
+}
+
+variable "parameters" {
+  default = {
+    "dev.expense.rds.username" = "admin1"
+  }
+}
+
+#Note: In organisation we don't keep passwords in git repo, here for learning purpose
+variable "secrets" {
+  default = {
+    "dev.expense.rds.password" = "ExpenseApp123"
+  }
+}
