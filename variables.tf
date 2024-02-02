66843c2de79b9a23a8a637fe9ee5d4ed7d87f0d7
rakesh Guddeti <raki64846@gmail.com>
2024-02-02 19:44:03 +0530

    .

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "key_id" {
 variable "parameters" {
   default = {
     "dev.expense.rds.username" = "admin1"
+    "dev.expense.rds.endpoint" = "dev-expense-rds.ckulorxm5mjc.us-east-1.rds.amazonaws.com"
   }
 }
 
